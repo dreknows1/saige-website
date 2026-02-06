8321a84dd8b72320fc07e130345f63ac26a1f325
Eddie Volt <eddievolt@Eddies-Mac-mini.local>
2026-02-06 18:01:15 -0500

    Restore full Saige website with admin backend

--- src/main.tsx
+++ src/main.tsx
@@ -1,25 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import App from './App'
+import { AudioPlayerProvider } from './contexts/AudioPlayerContext'
 import './index.css'
-
-function App() {
-  return (
-    <div style={{ 
-      minHeight: '100vh', 
-      background: '#0a0a0f', 
-      color: 'white',
-      padding: '40px',
-      fontFamily: 'system-ui'
-    }}>
-      <h1 style={{ color: '#ff0080' }}>SAIGE WEBSITE</h1>
-      <p>Site is loading correctly!</p>
-      <p>Admin: <a href="#/admin" style={{ color: '#00ffff' }}>Go to Admin</a></p>
-    </div>
-  )
-}
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <AudioPlayerProvider>
+      <App />
+    </AudioPlayerProvider>
   </React.StrictMode>,
 )
